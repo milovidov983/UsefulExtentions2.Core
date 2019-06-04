fa4a4911142e604432d23c2fa5eb6762f8f36dd5
Vasiliy Milovidov <milovidov.vasya@gmail.com>
2019-06-04 15:32:45 +0300

    Add 2 tests to DuffExtention

--- Tests/UsefulExtention2.Core.Tests/DiffExtensionsTests.cs
+++ Tests/UsefulExtention2.Core.Tests/DiffExtensionsTests.cs
@@ -14,7 +14,25 @@ namespace UsefulExtention2.Core.Tests {
 
 			var result = left.DiffTo(right, StringComparer.InvariantCulture, StringComparer.InvariantCulture);
 
+
 			Assert.Equal(6, result.Count());
+			Assert.Equal(3, result.Select(x => x.Left).Intersect(left).Count());
+			Assert.Equal(3, result.Select(x => x.Right).Intersect(right).Count());
+		}
+
+		[Fact]
+		public void DiffTo_SetTwoCollectionsWithOneSameElements_ReturnOnlyUniqElements() {
+			var left = new[] { "a", "b", "c" };
+			var right = new[] { "a", "b1", "c1" };
+
+			var result = left.DiffTo(right, StringComparer.InvariantCulture, StringComparer.InvariantCulture);
+
+			var leftExpected = new[] { "b", "c" };
+			var rightExpected = new[] { "b1", "c1" };
+
+			Assert.Equal(4, result.Count());
+			Assert.Equal(2, result.Select(x => x.Left).Intersect(leftExpected).Count());
+			Assert.Equal(2, result.Select(x => x.Right).Intersect(rightExpected).Count());
 		}
 	}
 }
